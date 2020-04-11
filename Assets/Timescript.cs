0adfb9daebb87142e32a38a5474b3daa5e4c1809
baauliyaulafifah177 <18523177@students.uii.ac.id>
2020-04-11 01:41:08 -0800

    MenuScene dan GameOverscene

--- Assets/Timescript.cs
+++ Assets/Timescript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timescript : MonoBehaviour
 {
@@ -26,5 +27,7 @@ public class Timescript : MonoBehaviour
             counter -= 1;
             timerText.text = counter.ToString();
         }      
+        SceneManager.LoadScene("GameOverScene");
+
     }
 }
